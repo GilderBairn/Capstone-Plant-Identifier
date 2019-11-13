7d5485e62995d5527ef260382718c3329dfb421c
GilderBairn <benjaminabbott3@gmail.com>
2019-11-13 12:46:50 -0500

    modified plant_data to use batch predicting, has memory leaks.

--- plant_data.py
+++ plant_data.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import cv2
 import os
 import tensorflow as tf
@@ -23,37 +21,45 @@ def get_inceptionv3():
 plant_data_dir = '/media/ben/DATA/plantdata'
 
 
-def compile_plant_data(input_dir, output_file):
+def compile_plant_data(input_dir, output_file, parallel_num):
     source_dir = os.path.join(plant_data_dir, input_dir)
     counter = 0
     dataset = []
     inception = get_inceptionv3()
-    for file in os.listdir(source_dir):
-        # find xml files in the directory
-        if file.endswith('.xml'):
-            if counter % 100 == 0:
-                print(counter)
-            # read class label from xml file, and get image from specified jpg with name mediaID
-            with open(os.path.join(source_dir, file), 'r') as fp:
-                bs = BeautifulSoup(fp.read(), features='html.parser')
-            mediaID = bs.mediaid.get_text()
-            image = cv2.imread(os.path.join(source_dir, mediaID + '.jpg'))
-            label = bs.classid.get_text()
-            # preprocess image and feed to inception for feature generation
-            image = preprocess(image)
-            image = tf.data.Dataset.from_tensors([image])
-            features = inception.predict(image, verbose=0, steps=1)[0]
-            # build the new dict of features and label, and add it to the dataset list
-            example = {}
-            for i, feature in enumerate(features):
-                example['feature' + str(i)] = feature
-            example['label'] = label
-            dataset.append(example)
-            counter += 1
-    # write the dataset to a json for future learning
-    with open(os.path.join(source_dir, output_file)) as out:
-        json.dump(dataset, out)
+    label_list = []
+    image_list = []
+    examples = os.listdir(source_dir)
+    with open(os.path.join(plant_data_dir, output_file), 'w') as out:
+        for file in examples:
+            if file.endswith('xml'):
+                # find xml files in the directory
+                if counter % 100 == 0:
+                    print(counter)
+                # read class label from xml file, and get image from specified jpg with name mediaID
+                with open(os.path.join(source_dir, file), 'r') as fp:
+                    bs = BeautifulSoup(fp.read(), features='html.parser')
+                mediaID = bs.mediaid.get_text()
+                image_list.append(preprocess(cv2.imread(os.path.join(source_dir, mediaID + '.jpg'))))
+                label_list.append(bs.classid.get_text())
+                # fee images into inception in groups to improve total runtime
+                if len(image_list) >= parallel_num:
+                    feature_list = inception.predict(tf.data.Dataset.from_tensors(image_list), verbose=1, steps=1)
+                    # build the new dict of features and label, and add it to the dataset list
+                    example = {}
+                    for data, label in zip(feature_list, label_list):
+                        for i, feature in enumerate(data):
+                            example['feature_' + str(i)] = float(feature)
+                        example['label'] = int(label)
+                        dataset.append(example)
+                    image_list.clear()
+                    # clear tracker lists and write to outfile
+                    json.dump(dataset, out)
+                    dataset.clear()
+                    label_list.clear()
+                    example.clear()
+                counter += 1
 
 
+tf.executing_eagerly()
 target_directories = ['test', 'train']
-compile_plant_data('train', 'train_compiled.json')
+compile_plant_data('train', 'train_compiled.json', 8)
